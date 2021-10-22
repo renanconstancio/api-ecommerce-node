2a9c0762d2c6b9a0044a8e98784d76d5148a00d3
Renan Constancio <renanhenrique86@hotmail.com>
2021-10-22 16:19:13 -0300

    Ajustes para as rotas

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import marcasRouter from '@modules/marca/routes/marcas.routes';
 import clientesRouter from '@modules/cliente/routes/clientes.routes';
 import produtosRouter from '@modules/produtos/routes/produtos.routes';
 import produtoSkusRouter from '@modules/produtos_skus/routes/produtos_skus.routes';
+import produtoImgsRouter from '@modules/produtos_imgs/routes/produtos_imgs.routes';
 // import productsRouter from '@modules/produto/routes/products.routes';
 // import usersRouter from '@modules/users/routes/users.routes';
 // import sessionsRouter from '@modules/users/routes/sessions.routes';
@@ -17,5 +18,6 @@ routes.use('/v1/marcas', marcasRouter);
 routes.use('/v1/clientes', clientesRouter);
 routes.use('/v1/produtos', produtosRouter);
 routes.use('/v1/produtos_skus', produtoSkusRouter);
+routes.use('/v1/produtos_imgs', produtoImgsRouter);
 
 export default routes;
