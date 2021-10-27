8581aab6c71cc472b49f3731a74be2d105611d32
Renan Constancio <renanhenrique86@hotmail.com>
2021-10-27 11:21:42 -0300

    Alterações no multer para upload

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
+import AppError from '@shared/errors/AppError';
 
 interface IUploadConfig {
   driver: 's3' | 'disk';
@@ -24,12 +25,24 @@ export default {
   directory: uploadFolder,
   tmpFolder,
   multer: {
+    limits: { fileSize: 2000000 },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    fileFilter(req: any, file: { mimetype: string }, cb: any) {
+      if (!file.mimetype.match(/image.*/)) {
+        cb(
+          new AppError('Image uploaded is not of type jpg/jpeg/png or gif'),
+          false,
+        );
+      } else {
+        cb(null, true);
+      }
+    },
     storage: multer.diskStorage({
       destination: tmpFolder,
       filename(request, file, callback) {
-        const fileHash = crypto.randomBytes(10).toString('hex');
-
-        const filename = `${fileHash}-${file.originalname}`;
+        const fileHash = crypto.randomBytes(16).toString('hex');
+        const ext = path.extname(file.originalname);
+        const filename = `${fileHash}${ext}`;
 
         callback(null, filename);
       },
@@ -40,4 +53,4 @@ export default {
       bucket: 'api-vendas',
     },
   },
-} as IUploadConfig;
+} as unknown as IUploadConfig;
