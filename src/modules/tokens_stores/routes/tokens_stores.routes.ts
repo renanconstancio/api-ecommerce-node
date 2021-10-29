b5bf1ba19425f43a1608089324ddbb15edfea78f
Renan Constancio <renanhenrique86@hotmail.com>
2021-10-29 16:35:09 -0300

    modificação dos tokens para a api

--- src/modules/tokens_stores/routes/tokens_stores.routes.ts
+++ src/modules/tokens_stores/routes/tokens_stores.routes.ts
@@ -5,19 +5,7 @@ import TokensStoresControllers from '../controllers/TokensStoresControllers';
 const tokensRouter = Router();
 const tokensController = new TokensStoresControllers();
 
-// usersRouter.use(isAuthenticated);
-
 tokensRouter.get('/', tokensController.index);
-
-// usersRouter.get(
-//   '/:id',
-//   celebrate({
-//     [Segments.PARAMS]: {
-//       id: Joi.number().required(),
-//     },
-//   }),
-//   usersController.show,
-// );
 
 tokensRouter.post(
   '/',
