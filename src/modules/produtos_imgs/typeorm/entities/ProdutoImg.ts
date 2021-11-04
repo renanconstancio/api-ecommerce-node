08a40afab78c0e3685c93ac2b5c65c78e7926d69
Renan Constancio <renanhenrique86@hotmail.com>
2021-11-04 10:50:28 -0300

    ajuste nas imagens

--- src/modules/produtos_imgs/typeorm/entities/ProdutoImg.ts
+++ src/modules/produtos_imgs/typeorm/entities/ProdutoImg.ts
@@ -2,12 +2,16 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { Exclude } from 'class-transformer';
+import ProdutoSku from '@modules/produtos_skus/typeorm/entities/ProdutoSku';
+import Produto from '@modules/produtos/typeorm/entities/Produto';
 
-@Entity('produtos_imgs')
+@Entity('produtos_imgs', { orderBy: { ordem: 'ASC' } })
 class ProdutoImg {
   @PrimaryGeneratedColumn()
   id: number;
@@ -33,6 +37,14 @@ class ProdutoImg {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @OneToOne(() => Produto, (prod: Produto) => prod.prodimgs)
+  @JoinColumn({ name: 'id_produtos' })
+  product: Produto;
+
+  @OneToOne(() => ProdutoSku, (skus: ProdutoSku) => skus.skuimgs)
+  @JoinColumn({ name: 'id_produtos_skus' })
+  skus: ProdutoSku;
 }
 
 export default ProdutoImg;
