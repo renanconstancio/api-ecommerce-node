64a25a4f41424d855be3301f22f2edd87e8eb919
Renan Constancio <renanhenrique86@hotmail.com>
2021-11-06 10:44:05 -0300

    Ajustes no service

--- src/modules/produtos/services/ShowProdutoService.ts
+++ src/modules/produtos/services/ShowProdutoService.ts
@@ -11,13 +11,30 @@ class ShowProdutoService {
   public async execute({ id }: IRequest, connect: string): Promise<Produto> {
     const produtosRepository = getCustomRepository(ProdutoRepository, connect);
 
-    const produto = await produtosRepository.findOne(id);
+    const produto = await produtosRepository
+      .createQueryBuilder('Prod')
+      .innerJoinAndSelect('Prod.brand', 'BrandP')
+      .innerJoinAndSelect('Prod.skus', 'SkuP')
+      .leftJoinAndSelect(
+        'SkuP.skuimgs',
+        'SkuPImg',
+        'SkuPImg.id_produtos_skus IS NOT NULL',
+      )
+      .leftJoinAndSelect(
+        'Prod.prodimgs',
+        'ProdImg',
+        'ProdImg.id_produtos_skus IS NULL',
+      )
+      .where('Prod.id = :id', { id: id })
+      .getOne();
+
+    // const produto = await produtosRepository.findOne(id);
 
     if (!produto) {
       throw new AppError('Product not found.');
     }
 
-    return produto;
+    return produto as Produto;
   }
 }
 
