2de13907413cb52d64e9ff4f016985e54bea6cd8
Renan Constancio <renanhenrique86@hotmail.com>
2021-11-06 10:46:17 -0300

    Ajustes em types

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,8 +1,11 @@
 declare namespace Express {
   export interface Request {
+    // string setada dentro do express
     user: {
       id: string;
     };
+    // string setada dentro do express
+    // vale de resaltar que pega a conexao do banco de dados
     connect: string;
   }
 }
