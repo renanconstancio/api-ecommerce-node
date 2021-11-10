b32d9b479d70820faf7ca4c430c19e73b9b89cf6
Renan Constancio <renanhenrique86@hotmail.com>
2021-11-10 11:16:50 -0300

    ajustes e tratativa de erro para celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,19 +3,13 @@ import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import { errors } from 'celebrate';
+import { isCelebrateError } from 'celebrate';
 import { pagination } from 'typeorm-pagination';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
-import { ValidationError } from 'joi';
 import '@shared/typeorm';
-
 import uploadConfig from '@config/upload';
 // import rateLimiter from '@shared/http/middlewares/rateLimiter';
-
-interface ValidationErrors {
-  [key: string]: string[];
-}
 
 const app = express();
 
@@ -27,31 +21,42 @@ app.use(express.json());
 app.use(pagination);
 
 app.use('/files', express.static(uploadConfig.directory));
+
 app.use(routes);
 
-app.use(errors());
+// app.use(errors());
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof ValidationError) {
-      const errors: ValidationErrors = {};
+    if (isCelebrateError(error)) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const validation: any = {};
+      const errorBodyDetails = error.details.get('body')?.details || {};
 
-      error.details.forEach(err => {
-        errors[err.path] = err.errors;
+      Object.entries(errorBodyDetails).forEach(
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ([, value]: [string, any | unknown | null]): void => {
+          validation[['field', value.path].join('.')] = value.message;
+        },
+      );
+
+      return response.status(400).json({
+        statusCode: 400,
+        status: 'error',
+        message: validation,
       });
-
-      return response.status(400).json({ message: 'validation fails', errors });
     }
 
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
+        statusCode: error.statusCode,
         status: 'error',
         message: error.message,
       });
     }
 
-    console.log('Error Server: %O', error);
+    // console.log('Error: %O', error);
 
     return response.status(500).json({
       status: 'error',
